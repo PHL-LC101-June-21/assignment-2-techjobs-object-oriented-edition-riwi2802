af16910a78ccf87d98ca45962893fd91f6366f5b
Richard WIlliams <riwi2802@colorado.edu>
2021-10-15 14:29:41 -0400

    Added constructor in location class to initialize id field & initialize value field
    added getter and setters for CoreCOmpetency class id and value fields
    
    added a tostring method in the PositionType Class & added equals & hashcode methods
    
    added testJobConstructorSetsAllFields test function

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,12 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import org.launchcode.techjobs.oo.*;
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -9,4 +14,27 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+
+
+    @Test
+    public void testSettingJobID() {
+        Job object1 = new Job();
+        Job object2 = new Job();
+        assertNotEquals(object1, object2);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+       Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+       assertTrue(testJob instanceof  Job);
+       assertEquals(testJob.getName(),"Product tester");
+        assertTrue(testJob.getEmployer() instanceof  Employer);
+        assertEquals(testJob.getEmployer().getValue(), "ACME" );
+        assertTrue(testJob.getLocation() instanceof Location);
+        assertEquals(testJob.getLocation().getValue(),"Desert");
+        assertTrue(testJob.getPositionType() instanceof  PositionType);
+        assertEquals(testJob.getPositionType().getValue(), "Quality control" );
+        assertTrue(testJob.getCoreCompetency() instanceof  CoreCompetency);
+        assertEquals(testJob.getCoreCompetency().getValue(), "Persistence" );
+    }
 }
