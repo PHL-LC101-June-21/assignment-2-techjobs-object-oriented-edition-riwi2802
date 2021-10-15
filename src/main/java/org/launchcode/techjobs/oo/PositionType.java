99e4d43d2374ad504e5796e7bfe2dea795d07ebb
Richard WIlliams <riwi2802@colorado.edu>
2021-10-15 10:22:58 -0400

    Added constructor in location class to initialize id field & initialize value field
    added getter and setters for CoreCOmpetency class id and value fields
    
    added a tostring method in the PositionType Class & added equals & hashcode methods

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Objects;
+
 public class PositionType {
 
     private int id;
@@ -18,10 +20,30 @@ public class PositionType {
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
+    // Should I be returning this.value or value??? & should I note @Override here?*
+    @Override
+    public String toString() {
+        return "Position Type : " + value;
+    }
+
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
 
     // Getters and Setters:
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PositionType that = (PositionType) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
     public int getId() {
         return id;
